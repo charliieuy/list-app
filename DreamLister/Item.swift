64c307c00689a3a597c0de91f7ea359dc3c76b38
Carlos Uy <charliieuy@gmail.com>
2017-08-12 15:39:22 -0400

    commit project

---
+++ DreamLister/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  DreamLister
+//
+//  Created by Carlos Uy on 8/10/17.
+//  Copyright © 2017 Orangebox Technology LLC. All rights reserved.
+//
+
+import Foundation
---
+++ DreamLister/MaterialView.swift
@@ -0,0 +1,21 @@
+//
+//  MaterialView.swift
+//  DreamLister
+//
+//  Created by Carlos Uy on 8/10/17.
+//  Copyright © 2017 Orangebox Technology LLC. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
